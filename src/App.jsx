b0bc9d61627c60f0f3f7ff2ffb2a7035964bb717
qiqzhao <bme_ritter@foxmail.com>
2024-08-23 10:54:33 +0800

    chore: init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button, Label } from "@qiqzhao/choco-ui";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      helo
+      <Button> click</Button>
+      <Label> click</Label>
+    </div>
+  );
+}
+
+export default App;
